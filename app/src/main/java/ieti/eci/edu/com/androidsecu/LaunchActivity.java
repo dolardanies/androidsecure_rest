026a489bc3f7c405377a699cada3a359e5f30e21
d.castiblanco <dolardanies@hotmail.com>
2019-04-12 22:10:07 -0500

    P1 7-8i

--- app/src/main/java/ieti/eci/edu/com/androidsecu/LaunchActivity.java
+++ app/src/main/java/ieti/eci/edu/com/androidsecu/LaunchActivity.java
@@ -3,4 +3,19 @@ package ieti.eci.edu.com.androidsecu;
 import android.support.v7.app.AppCompatActivity;
 
 public class LaunchActivity extends AppCompatActivity {
+    public static final String TOKEN_KEY = "TOKEN_KEY";
+
+    @Override
+    protected void onCreate( @Nullable Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+        SharedPreferences sharedPref =
+                getSharedPreferences( getString( R.string.preference_file_key ), Context.MODE_PRIVATE );
+
+        if(sharedPref.contains(TOKEN_KEY)){
+            //TODO go to MainActivity
+        }else{
+            //TODO go to LoginActivity
+        }
+    }
 }
